28ec0f337ae9b1ccb0a623be971f443436e94f84
Tryphene <73878457+Tryphene@users.noreply.github.com>
2023-06-03 17:44:43 +0000

    Update affichage_participant.php

--- affichage_participant.php
+++ affichage_participant.php
@@ -2,14 +2,6 @@
 header('Access-Control-Allow-Origin: *');
 include 'header.php';
 
-//  if(!empty($_POST["dteCourse"])){
-// 	$req = $bdd->prepare("SELECT * FROM `course` WHERE `dteCourse` LIKE :dte");
-// 	$req->bindParam(':dte', $_POST["dteCourse"]);
-//     $req->execute();
-// } else {
-// 	$req = $bdd->prepare("SELECT * FROM `course` ORDER BY `id` DESC");
-//     $req->execute();
-// }
 $req = $bdd->prepare("SELECT * FROM `participant` ORDER BY `id` DESC");
 $req->execute();
 
